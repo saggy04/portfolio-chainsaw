da00b73f94af72311c7d0b7d080825c96d5b533b
Sagnick Biswas <49414958+saggy04@users.noreply.github.com>
2020-11-27 18:18:50 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,102 @@
+
+window.onload = function(){
+  document.querySelector('.loader_bg').style.display = "none";
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onscroll = function() {myFunction()};
+// window.onload = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+window.onload = function(){
+  document.querySelector('.contact-form-btn').addEventListener('click', function(){
+      const name = document.querySelector('.firstName').value;
+      localStorage.setItem('textvalue', name);
+  });
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+}
+
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+
+function updateVisitCount() {
+  // fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
+  fetch('https://api.countapi.xyz/update/thisissagnick.tech/fbfcd2c8-1f9d-4160-bed4-cc8572211728/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
+
+document.querySelector('.contact-form-btn').addEventListener('click', function(){
+  const name = document.querySelector('.firstName').value;
+  localStorage.setItem('textvalue', name);
+});
+}
